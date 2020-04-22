7bb559a356a513e85900a0d14720c1ef709ca800
Akshat Aggarwal <akshat.aggarwal@nagarro.com>
2020-04-22 17:26:41 +0530

    complete project

---
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -0,0 +1,25 @@
+import { Component} from '@angular/core';
+import { AssignmentComponent } from '../../assignment.component';
+
+
+
+@Component({
+  selector: 'app-assignment2',
+  templateUrl: './assignment2.component.html',
+  styleUrls: ['./assignment2.component.css']
+})
+export class Assignment2Component {
+  todos:any;
+  constructor(ar: AssignmentComponent)
+  {
+    this.todos = ar.getTodos();
+  }
+  selectedIndex: number;
+  select(index: number) {
+      this.selectedIndex = index;
+  }
+
+
+
+
+}
